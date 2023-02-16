7621072dd7fdaa7eaa6cce91eb5864696fe47b07
santi-caballero <santiago.ph17@hotmail.com>
2023-02-16 02:43:27 -0300

    Single title info working with modal

--- commons/card.jsx
+++ commons/card.jsx
@@ -1,14 +1,16 @@
 import "@picocss/pico";
 
 const Card = ({ title }) => {
+  // console.log(title);
   return (
     <>
       <div style={{ width: 300, height: 520 }}>
-        {/* <p>{title.title}</p> */}
         <img
           style={{ height: "100%" }}
           src={`https://image.tmdb.org/t/p/original${title.poster_path}`}
-        />
+          // data-target="modal-example"
+          // onClick={toggleModal}
+        ></img>
       </div>
     </>
   );
