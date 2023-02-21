16678fae636d24b7f3f2d65dbd6cdb421b6e2b1a
santi-caballero <santiago.ph17@hotmail.com>
2023-02-20 23:46:48 -0300

    Arreglos en el modal

--- commons/card.jsx
+++ commons/card.jsx
@@ -1,13 +1,37 @@
 import "@picocss/pico";
+import { useState } from "react";
+import Modal from "./modal";
 
 const Card = ({ title }) => {
+  const [data, setData] = useState();
+
+  const toggleModal = (event) => {
+    setData({
+      title: title.title || title.name,
+      clasificacion: title.vote_average,
+      descripcion: title.overview || "No disponible",
+    });
+  };
+
   return (
     <>
       <div style={{ width: 300, height: 520 }}>
         <img
           style={{ height: "100%", paddingBottom: 40 }}
           src={`https://image.tmdb.org/t/p/original${title.poster_path}`}
+          data-target="modal-example"
+          onClick={toggleModal}
         ></img>
+        <Modal
+          data={data}
+          close={() => setData(null)}
+          // title={title}
+          // clasificacion={clasificacion}
+          // descripcion={descripcion}
+          // title={title.title ? title.title : title.name}
+          // clasificacion={title.vote_average}
+          // descripcion={title.overview ? title.overview : "No disponible"}
+        />
       </div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from "next/head";
-import Card from "@/commons/card";
+
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { initFirebase } from "@/firebase/firebaseApp";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
-import { useState } from "react";
-import Modal from "@/commons/modal";
+
 import SearchBar from "../components/SearchBar";
+import Card from "@/commons/card";
 
 export default function Home({ popularMovies, popularSeries }) {
   initFirebase();
@@ -14,83 +14,12 @@ export default function Home({ popularMovies, popularSeries }) {
   const auth = getAuth();
   const router = useRouter();
   const [user, loading, error] = useAuthState(auth);
-  const [title, setTitle] = useState();
-  const [clasificacion, setClasificacion] = useState();
-  const [descripcion, setDescripcion] = useState();
 
   const login = async () => {
     const result = await signInWithPopup(auth, provider);
     // console.log(result.user);
   };
-  const isOpenClass = "modal-is-open";
-  const openingClass = "modal-is-opening";
-  const closingClass = "modal-is-closing";
-  const animationDuration = 400; // ms
-  let visibleModal = null;
 
-  const toggleModal = (event) => {
-    event.preventDefault();
-    const modal = document.getElementById(
-      event.currentTarget.getAttribute("data-target")
-    );
-    typeof modal != "undefined" && modal != null && isModalOpen(modal)
-      ? closeModal(modal)
-      : openModal(modal);
-  };
-
-  const isModalOpen = (modal) => {
-    return modal.hasAttribute("open") && modal.getAttribute("open") != "false"
-      ? true
-      : false;
-  };
-
-  const openModal = (modal) => {
-    if (isScrollbarVisible()) {
-      document.documentElement.style.setProperty(
-        "--scrollbar-width",
-        `${getScrollbarWidth()}px`
-      );
-    }
-    document.documentElement.classList.add(isOpenClass, openingClass);
-    setTimeout(() => {
-      visibleModal = modal;
-      document.documentElement.classList.remove(openingClass);
-    }, animationDuration);
-    modal.setAttribute("open", true);
-  };
-
-  const closeModal = (modal) => {
-    visibleModal = null;
-    document.documentElement.classList.add(closingClass);
-    setTimeout(() => {
-      document.documentElement.classList.remove(closingClass, isOpenClass);
-      document.documentElement.style.removeProperty("--scrollbar-width");
-      modal.removeAttribute("open");
-    }, animationDuration);
-  };
-  const isScrollbarVisible = () => {
-    return document.body.scrollHeight > screen.height;
-  };
-  const getScrollbarWidth = () => {
-    // Creating invisible container
-    const outer = document.createElement("div");
-    outer.style.visibility = "hidden";
-    outer.style.overflow = "scroll"; // forcing scrollbar to appear
-    outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
-    document.body.appendChild(outer);
-
-    // Creating inner element and placing it in the container
-    const inner = document.createElement("div");
-    outer.appendChild(inner);
-
-    // Calculating difference between container's full width and the child width
-    const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
-
-    // Removing temporary elements from the DOM
-    outer.parentNode.removeChild(outer);
-
-    return scrollbarWidth;
-  };
   return (
     <>
       <Head>
@@ -112,19 +41,7 @@ export default function Home({ popularMovies, popularSeries }) {
               <tr>
                 {popularMovies.results.map((movie, i) => (
                   <th key={i}>
-                    <div style={{ width: 300, height: 520 }}>
-                      <img
-                        style={{ height: "100%" }}
-                        src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                        data-target="modal-example"
-                        onClick={(event) => {
-                          setTitle(movie.title);
-                          setClasificacion(movie.vote_average);
-                          setDescripcion(movie.overview);
-                          toggleModal(event);
-                        }}
-                      ></img>
-                    </div>
+                    <Card title={movie} />
                   </th>
                 ))}
               </tr>
@@ -141,32 +58,13 @@ export default function Home({ popularMovies, popularSeries }) {
                 {popularSeries.results.map((serie, j) => (
                   <th key={j}>
                     <div style={{ width: 300, height: 520 }}>
-                      <img
-                        style={{ height: "100%" }}
-                        src={`https://image.tmdb.org/t/p/original${serie.poster_path}`}
-                        data-target="modal-example"
-                        onClick={(event) => {
-                          setTitle(serie.name);
-                          setClasificacion(serie.vote_average);
-                          setDescripcion(serie.overview);
-                          toggleModal(event);
-                        }}
-                      ></img>
+                      <Card title={serie} />
                     </div>
                   </th>
                 ))}
               </tr>
             </thead>
           </table>
-          <Modal
-            toggleModal={toggleModal}
-            title={title}
-            clasificacion={clasificacion}
-            descripcion={descripcion}
-            // title={title.title ? title.title : title.name}
-            // clasificacion={title.vote_average}
-            // descripcion={title.overview ? title.overview : "No disponible"}
-          />
         </figure>
       </div>
     </>
